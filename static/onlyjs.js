1ab8f994d5461f2361c9035320e360633d6dd9a5
dwu1547 <dw1547@nyu.edu>
2017-08-16 01:02:48 -0400

    proc

--- static/onlyjs.js
+++ static/onlyjs.js
@@ -8,7 +8,7 @@ if(String(ranksolo).includes("Unranked") == false) {
 var getsolo = ranksolo.split("_");
 document.getElementById("cardsolotier").innerHTML = getsolo[0];
 document.getElementById("cardsolorank").innerHTML = getsolo[1];
-document.getElementById("soloimg").src = ("static/badges/" + String(getsolo[0]) + "_" + String(getsolo[1]) +"_badge.png");
+document.getElementById("soloimg").src = "static/badges/" + String(getsolo[0]) + "_" + String(getsolo[1]) +"_badge.png";
 
 }else{
 document.getElementById("cardsolotier").innerHTML = "Unranked";
@@ -22,12 +22,12 @@ if(String(rankflex).includes("Unranked") == false) {
 var getflex = rankflex.split("_");
 document.getElementById("cardflextier").innerHTML = getflex[0];
 document.getElementById("cardflexrank").innerHTML = getflex[1];
-document.getElementById("fleximg").src = ("static/badges/" + String(getflex[0]) + "_" + String(getflex[1]) +"_badge.png");
+document.getElementById("fleximg").src = "static/badges/" + String(getflex[0]) + "_" + String(getflex[1]) +"_badge.png";
 
 }else{
 document.getElementById("cardflextier").innerHTML = "Unranked";
 document.getElementById("cardflexrank").innerHTML = "NaN";
-document.getElementById("fleximg").src = "static/badges/unranked_badge.png";
+document.getElementById("fleximg").src = "static/badges/" + "unranked_badge.png";
 }
 
 //transfer ranks to card (TT)
@@ -36,7 +36,7 @@ if(String(ranktt).includes("Unranked") == false) {
 var gettt = ranktt.split("_");
 document.getElementById("cardtttier").innerHTML = gettt[0];
 document.getElementById("cardttrank").innerHTML = gettt[1];
-document.getElementById("ttimg").src = ("static/badges/" + String(gettt[0]) + "_" + String(gettt[1]) +"_badge.png");
+document.getElementById("ttimg").src = "static/badges/" + String(gettt[0]) + "_" + String(gettt[1]) +"_badge.png";
 
 }else{
 document.getElementById("cardtttier").innerHTML = "Unranked";
