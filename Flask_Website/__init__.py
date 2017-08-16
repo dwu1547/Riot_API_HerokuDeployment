1dece4532ec24c4c5ab16fd01e084850c3c456bf
dwu1547 <dw1547@nyu.edu>
2017-08-15 22:06:27 -0400

    first heroku attempt

--- Flask_Website/__init__.py
+++ Flask_Website/__init__.py
@@ -72,9 +72,9 @@ def my_form_get():
     #else:
         #return str(error)
 
-
+#app.run(host="127.0.0.1",port=5000,debug=True)
 if __name__=="__main__":
-    app.run(host="127.0.0.1",port=5000,debug=True)
+    app.run(debug=True)
 
 
 
